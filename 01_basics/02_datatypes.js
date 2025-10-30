72666e065324b4661483b5ac8260dd309d81c297
Aminur Rahman <ar2069413@gmail.com>
2025-10-30 09:28:42 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,59 @@
+"use strict"; // treat all JS code code as newer version
+
+// alert(3 + 3) // I'm using nodejs, not browser
+
+console.log(349 * 37);  // code readability should be high
+console.log("Aminur")
+
+let name = "Aminur"
+let age = 21
+let isLoggedIn = true
+let state = null
+
+// DATATYPES -
+// number => 2 to power 53
+// bigint
+// string =>
+// boolean => true/false
+// null => stand alone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Aminur");  //Types
+console.log(typeof age);
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
