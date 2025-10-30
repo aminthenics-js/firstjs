c124191735ac780dd3aa76ea63d02c151d754838
Aminur Rahman <ar2069413@gmail.com>
2025-10-30 07:11:23 +0000

    Const var let

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,50 @@
+const accountId = 7784343
+let accountEmail = "hitesh@google.com"
+var accountPassword = "9876"
+accountCity = "Sylhet"
+let accountState;
+
+
+// accountId = 2 // not allowed // Prefer to use let keyword because const can't be changed
+
+
+accountEmail = "huhu@gmail.com"
+accountPassword = "1234"
+accountCity = "Dhaka"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use VAR 
+Because of issue in block scope & functional scope
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
