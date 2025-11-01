98c49fbf7732d0e7e469f6b3ed30c1cfed7613e2
Aminur Rahman <ar2069413@gmail.com>
2025-11-01 14:56:17 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,96 @@
+console.log("")
+
+let score = "67"
+let value = 60
+
+console.log(typeof score);
+console.log(typeof (value));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+
+let merit = "439ryf9e438"   // NaN - Not a Number
+
+console.log(typeof merit);
+
+let meritInNumber = Number(merit)
+console.log(typeof meritInNumber);
+console.log(meritInNumber);
+
+
+
+
+let worth = null
+
+console.log(typeof worth);
+
+let worthInNumber = Number(worth)
+console.log(typeof worthInNumber);
+console.log(worthInNumber);
+
+
+
+
+let gain = true
+
+console.log(typeof gain);
+
+let gainInNumber = Number(gain)
+console.log(typeof gainInNumber);
+console.log(gainInNumber);
+
+
+
+
+
+let use = false
+
+console.log(typeof use);
+
+let useInNumber = Number(use)
+console.log(typeof useInNumber);
+console.log(useInNumber);
+
+
+
+
+
+let name = "Aminthenics"
+
+console.log(typeof name);
+
+let nameInNumber = Number(name)
+console.log(typeof nameInNumber);
+console.log(nameInNumber);
+
+
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)  // 1 => true; 0 => false
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+
+
+let isLoggedOut = 0
+
+let booleanIsloggedOut = Boolean(isLoggedOut)
+console.log(typeof booleanIsloggedOut);
+console.log(booleanIsloggedOut);
+
+
+
+
+let someNumber = 9598
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber); 
