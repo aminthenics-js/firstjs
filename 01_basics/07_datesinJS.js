4e8690e64956b5c1b5614a2b457109ebccc9e88f
Aminur Rahman <ar2069413@gmail.com>
2025-11-09 18:43:47 +0000

    Date

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -1,7 +1,7 @@
 // Date 
 
 let myDate = new Date()
-/*
+
 console.log(myDate.toString());  /// Sun Nov 09 2025 18:04:42 GMT+0000 (Coordinated Universal Time)
 console.log(myDate.toDateString());
 console.log(myDate.toJSON());       // 2025-11-09T18:04:42.241Z
@@ -22,7 +22,7 @@ let myTimeStamp = Date.now()
 console.log(myTimeStamp);
 console.log(newCreatedDate.getTime());
 console.log(Math.round(Date.now()/1000));
-*/
+
 
 let newDate = new Date()
 console.log(newDate);
